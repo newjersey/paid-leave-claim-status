897ff802261a7f65c0d4c9ecd1daa7319b27c2e3
Matt Luedke <matthew@innovation.nj.gov>
2025-08-01 12:30:43 -0700

    employment page

--- src/claimApplication/identifyPage.js
+++ src/claimApplication/identifyPage.js
@@ -1,17 +1,19 @@
 import { identifyingContent as certificationContent } from "./claimantInfo/certification.js";
 import { identifyingContent as confirmationContent } from "./claimantInfo/confirmation.js";
 import { identifyingContent as employerDetailsContent } from "./claimantInfo/employerDetails.js";
+import { identifyingContent as employmentContent } from "./claimantInfo/employment.js";
 
 const identifyingContents = [
   certificationContent,
   confirmationContent,
   employerDetailsContent,
+  employmentContent,
 ];
 
 export function identifyPage() {
   for (const { id, elementId, text } of identifyingContents) {
     const element = document.getElementById(elementId);
-    if (element && element.textContent.includes(text)) {
+    if (element && element.offsetParent !== null && element.textContent.includes(text)) {
       return id;
     }
   }
