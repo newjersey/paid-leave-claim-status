7855dba20f0ca19c5412818bc2573c8e23d25994
Matt Luedke <matthew@innovation.nj.gov>
2025-08-06 08:44:32 -0700

    initial functions to replace header and add footer

--- src/claimApplication/tdiOverride.js
+++ src/claimApplication/tdiOverride.js
@@ -21,6 +21,8 @@ function executeOverride() {
 
   accessibilityChanges();
   applyGlobalFont();
+  replaceHeader();
+  applyFooter();
 }
 
 function applyGlobalFont() {
@@ -28,4 +30,31 @@ function applyGlobalFont() {
   document.querySelectorAll('*').forEach(element => {
     element.style.fontFamily = '"Public Sans", sans-serif';
   });
+}
+
+function replaceHeader() {
+  const tables = document.querySelectorAll('#form1 > table');
+  tables.forEach(table => {
+    const bannerImage = Array.from(table.querySelectorAll('img')).find(img => img.src.includes('lwd_banner.jpg'));
+    const logoImage = Array.from(table.querySelectorAll('img')).find(img => img.src.includes('lwd_logo.jpg'));
+    const hasLogoutLink = table.querySelector('a#header_lbtnLogout');
+
+    if (bannerImage && logoImage && hasLogoutLink) {
+      const newElement = document.createElement('h2');
+      newElement.textContent = 'Hello!';
+
+      table.replaceWith(newElement);
+    }
+  });
+}
+
+function applyFooter() {
+  const bodyContent = document.body;
+  if (bodyContent) {
+    const footerDiv = document.createElement('div');
+    footerDiv.innerHTML = "<h2>FOOTER</h2>";
+    bodyContent.appendChild(footerDiv);
+  } else {
+    console.error("Cannot find the body element to append the footer.");
+  }
 }
