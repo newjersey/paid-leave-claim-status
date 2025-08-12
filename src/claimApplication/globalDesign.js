332a917d808b387a8c191961695388c33049d2f0
Matt Luedke <matthew@innovation.nj.gov>
2025-08-12 05:41:21 -0700

    first version of info alert

--- src/claimApplication/globalDesign.js
+++ src/claimApplication/globalDesign.js
@@ -21,10 +21,38 @@ function replaceHeader() {
     const hasLogoutLink = table.querySelector('a#header_lbtnLogout');
 
     if (bannerImage && logoImage && hasLogoutLink) {
-      const newElement = document.createElement('p');
-      newElement.textContent = 'Hello!';
+      const bannerDiv = document.createElement('div');
+      bannerDiv.classList.add('usa-alert', 'usa-alert--info');
 
-      table.replaceWith(newElement);
+      const alertBodyDiv = document.createElement('div');
+      alertBodyDiv.classList.add('usa-alert__body');
+
+      const alertHeading = document.createElement('h4');
+      alertHeading.classList.add('usa-alert__heading');
+      alertHeading.textContent = 'Informative status';
+
+      const alertText = document.createElement('p');
+      alertText.classList.add('usa-alert__text');
+      alertText.innerHTML = 'Lorem ipsum dolor sit amet, <a class="usa-link" href="javascript:void(0);">consectetur adipiscing</a> elit, sed do eiusmod.';
+
+      alertBodyDiv.append(alertHeading, alertText);
+      bannerDiv.append(alertBodyDiv);
+      table.replaceWith(bannerDiv);
+
+      const styleElement = document.createElement('style');
+      styleElement.innerHTML = `
+        .usa-alert--info::before {
+          content: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0Ij48cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+PHBhdGggZD0iTTEyIDJDNi40OCAyIDIgNi40OCAyIDEyczQuNDggMTAgMTAgMTAgMTAtNC40OCAxMC0xMFMxNy41MiAyIDEyIDJ6bTEgMTVoLTJ2LTZoMnY2em0wLThoLTJWN2gydjJ6Ii8+PC9zdmc+');
+          display: inline-block;
+          width: 1em;
+          height: 1em;
+          margin-right: 0.5em;
+          background: none !important;
+          -webkit-mask: none !important;
+          mask: none !important;
+        }
+      `;
+      document.head.appendChild(styleElement);
     }
   });
 }
