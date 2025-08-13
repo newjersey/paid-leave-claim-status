dfea62ea9b345c3090be85d1bba957a62315ee5c
Matt Luedke <matthew@innovation.nj.gov>
2025-08-13 13:43:54 -0700

    logout svg in button

--- src/claimApplication/globalDesign.js
+++ src/claimApplication/globalDesign.js
@@ -52,6 +52,21 @@ function logoutHeader() {
   imgElement.style.marginRight = '18px';
 
   logoutHeader.appendChild(imgElement);
+  var buttonElement = document.createElement('button');
+  buttonElement.textContent = 'Logout';
+  buttonElement.classList.add('usa-button', 'usa-button--outline');
+
+  var iconElement = document.createElement('span');
+  iconElement.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/>
+    </svg>
+  `;
+
+  buttonElement.prepend(iconElement);
+
+  logoutHeader.appendChild(buttonElement);
 
   return logoutHeader;
 }
