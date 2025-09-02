d0523567656f00193f007466b6b7ff3ef1cb78e1
Matt Luedke <matthew@innovation.nj.gov>
2025-09-02 14:20:43 -0700

    text entries on employer

--- src/claimApplication/pageSpecificChanges.js
+++ src/claimApplication/pageSpecificChanges.js
@@ -1,11 +1,13 @@
 import { changes as certificationChanges, id as certificationId } from "./claimantInfo/certification";
 import { changes as confirmationChanges, id as confirmationId } from "./claimantInfo/confirmation";
+import { changes as employerDetailsChanges, id as employerDetailsId } from "./claimantInfo/employerDetails";
 
 export function pageSpecificChanges(pageId) {
   if (pageId === certificationId) {
     certificationChanges();
-  }
-  if (pageId === confirmationId) {
+  } else if (pageId === confirmationId) {
     confirmationChanges();
+  } else if (pageId === employerDetailsId) {
+    employerDetailsChanges();
   }
 }
