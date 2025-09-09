398c69e23d48b7528022da0acf06491592b5752a
Matt Luedke <matthew@innovation.nj.gov>
2025-09-09 13:52:13 -0700

    replace radio buttons on profile verification

--- src/claimApplication/claimantProfile/verification.js
+++ src/claimApplication/claimantProfile/verification.js
@@ -30,6 +30,7 @@ export const identifyingContent = {
 };
 
 export function changes() {
+  styleRadioButtons();
   adjustTable();
 }
 
@@ -58,5 +59,47 @@ function adjustTable() {
   if (repInputElement) {
     const repElement = repInputElement.parentElement;
     repElement.style.textAlign = 'left';
+  }
+}
+
+function styleRadioButtons() {
+  const parentContainer = document.getElementById('ContentPlaceHolder1_ClaimantProfileTab_tpnlVerification');
+
+  if (parentContainer) {
+    const tdElements = parentContainer.querySelectorAll('td');
+
+    tdElements.forEach(tdElement => {
+      if (tdElement.textContent.includes('My personal and contact information is correct')) {
+        const radioButtons = tdElement.querySelectorAll('input[type="radio"]');
+        const labels = tdElement.querySelectorAll('label');
+
+        const fieldset = document.createElement('fieldset');
+        fieldset.classList.add('usa-fieldset');
+        fieldset.style.marginBottom = '30px';
+
+        const legend = document.createElement('legend');
+        legend.classList.add('usa-legend');
+        legend.textContent = 'My personal and contact information is correct.';
+        fieldset.appendChild(legend);
+
+        radioButtons.forEach((radioButton, index) => {
+          const div = document.createElement('div');
+          div.classList.add('usa-radio');
+
+          const newRadioButton = radioButton.cloneNode(true);
+          newRadioButton.classList.add('usa-radio__input');
+
+          const newLabel = labels[index].cloneNode(true);
+          newLabel.classList.add('usa-radio__label');
+
+          div.appendChild(newRadioButton);
+          div.appendChild(newLabel);
+          fieldset.appendChild(div);
+        });
+
+        tdElement.innerHTML = '';
+        tdElement.appendChild(fieldset);
+      }
+    });
   }
 }
