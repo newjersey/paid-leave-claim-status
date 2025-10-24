dd251c1d69ed3ee15c79c00b6e5c2fcd50376a4a
Matt Luedke <matthew@innovation.nj.gov>
2025-10-24 06:54:28 -0700

    reword future alert

--- src/claimApplication/claimantInfo/confirmation.js
+++ src/claimApplication/claimantInfo/confirmation.js
@@ -661,19 +661,25 @@ function setNewTitleAndAlert() {
   title.textContent = `${i18next.t('confirmation.title')}`;
   document.removeEventListener('headerReady', setNewTitleAndAlert);
 
+  const isFuture = document.getElementById('ContentPlaceHolder1_ClaimantCertTab_TPConfirmation_btnFDDContinue') != null;
+  const alertText = isFuture 
+    ? i18next.t('confirmation.future_claim', { claim_id: claimId() }) 
+    : i18next.t('confirmation.submitted', { claim_id: claimId() });
+
   const alert = document.createElement('div');
   alert.classList.add("usa-alert", "usa-alert--info", "usa-alert--slim");
   alert.id = "submittedAlert";
   alert.innerHTML = `
     <div class="usa-alert__body">
       <p class="usa-alert__text">
-        ${i18next.t('confirmation.submitted', { claim_id: claimId() })}
+        ${alertText}
       </p>
     </div>
   `;
   title.parentNode.insertBefore(alert, title);
 
-  setupApplicationPdfDownloadLink()
+  setupApplicationPdfDownloadLink();
+  setupFuturePdfDownloadLink();
 }
 
 function setupApplicationPdfDownloadLink() {
@@ -684,10 +690,22 @@ function setupApplicationPdfDownloadLink() {
       logEvent('TDI Confirmation - PDF Download Clicked', {});
 
       const pdfDownloadBtn = document.getElementById('ContentPlaceHolder1_ClaimantCertTab_TPConfirmation_btnContinue');
-      const pdfFutureDownloadBtn = document.getElementById('ContentPlaceHolder1_ClaimantCertTab_TPConfirmation_btnFDDContinue');
       if (pdfDownloadBtn) {
         pdfDownloadBtn.click();
-      } else if (pdfFutureDownloadBtn) {
+      }
+    });
+  }
+}
+
+function setupFuturePdfDownloadLink() {
+  const downloadLink = document.getElementById('futurePdfDownload');
+  if (downloadLink) {
+    downloadLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      logEvent('TDI Confirmation - Future PDF Download Clicked', {});
+
+      const pdfFutureDownloadBtn = document.getElementById('ContentPlaceHolder1_ClaimantCertTab_TPConfirmation_btnFDDContinue');
+      if (pdfFutureDownloadBtn) {
         pdfFutureDownloadBtn.click();
       }
     });
