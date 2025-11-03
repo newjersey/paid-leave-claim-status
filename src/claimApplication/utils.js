ce40b8db4b781ff4127882d759c3587b1cd38670
Matt Luedke <matthew@innovation.nj.gov>
2025-11-03 11:28:57 -0800

    different syntax for set title

--- src/claimApplication/utils.js
+++ src/claimApplication/utils.js
@@ -175,9 +175,11 @@ export function replaceVerificationRadioButtons(
 }
 
 export function setNewTitle(text) {
-  document.addEventListener('headerReady', function () {
+  const setText = () => {
     const title = document.querySelector("#pageTitle");
     title.textContent = text;
-    document.removeEventListener('headerReady', arguments.callee);
-  });
+    document.removeEventListener('headerReady', setText);
+  };
+
+  document.addEventListener('headerReady', setText);
 }
