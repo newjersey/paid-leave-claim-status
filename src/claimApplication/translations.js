071b8801741fe466f2dcdd9287fb12233835da35
Jasmine Noor <jasminee@gmail.com>
2025-12-12 16:37:25 -0500

    #783 amending provider checkbox

--- src/claimApplication/translations.js
+++ src/claimApplication/translations.js
@@ -207,9 +207,10 @@ Please submit the form by {{est_deadline_date}} to avoid delays on my claim. Ple
           title: "Healthcare provider",
           explanation: "Your healthcare provider will need to confirm your medical condition and the start date of your disability.",
           info: "Healthcare provider information",
+          notAcceptedMessage:"Your application could get delayed or denied if it isn't certified by an approved healthcare provider.",
+          theseTypesProviders: "These types of healthcare providers can certify your leave:",
           type: {
             isAccepted: "My healthcare provider is one of the accepted types below.",
-            weAccept: "Providers we accept",
             advancedPracticeNurse: "Advanced Practice Nurse",
             registeredNurse: "Registered Nurse",
             certifiedNursePractitioner: "Certified Nurse Practitioner",
