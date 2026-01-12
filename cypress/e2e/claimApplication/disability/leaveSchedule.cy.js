1bed2d129ed402ec98045da687593e01a14bdd5a
John Chan <john@innovation.nj.gov>
2026-01-12 10:07:53 -0500

    add fix to yes and no buttons

--- cypress/e2e/claimApplication/disability/leaveSchedule.cy.js
+++ cypress/e2e/claimApplication/disability/leaveSchedule.cy.js
@@ -1,24 +1,13 @@
 import {
-  EXAMPLE_REASON_FOR_LEAVE_DATA_ILLNESS_DETAILS,
-  EXAMPLE_REASON_FOR_LEAVE_DATA_INJURY_DETAILS,
-  EXAMPLE_REASON_FOR_LEAVE_DATA_PREGNANCY,
-  EXAMPLE_REASON_FOR_LEAVE_DATA_PREGNANCY_DETAILS,
   globalTestsNew,
   globalTestsOld
 } from "../shared";
-import { encodeDecode } from '../../../../src/claimApplication/utils';
 
-const PAGE_ID = 'leaveSchedule';
+const PAGE_ID = 'disabilityInformation';
 const URL = 'ClaimantDisabililty';
 const FIXTURE = "./cypress/fixtures/claimApplication/disability/information.html";
 
 describe("Disability Information page", () => {
-  function checkPostData(interception) {
-    const formData = interception.request.body;
-    cy.checkCommonPostData(formData);
-    expect(formData).to.include('ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24txtDisStartDt=07%2F18%2F2025&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnDisStartDt=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnPregFlg=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnDtLDW=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnRTWFlg=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnDtRTW=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnDtExpRDTW=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24txtDtLastWorkd=07%2F17%2F2025&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24rbRec=rbtnRecYes&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24txtDtReturnedToWrk=08%2F17%2F2025&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24txtExpectedReturnedDtToWrk=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnConflictType=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24hdnTDIPayCode=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24Dis1%24btnSubmitConflictCheck=Continue&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24TabDoctor%24hdnInjCount=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24TabWC%24hdnNoClaimCount=&ctl00%24ContentPlaceHolder1%24ClaimantDisabilityTab%24tbpnlLatePayment%24hdnLPay=');
-  }
-
   describe("page without new JS", () => {
     beforeEach(() => {
       cy.intercept('**/tdiOverride.min.js', { body: '', disableCache: true }).as('scriptIntercept');
@@ -45,9 +34,10 @@ describe("Disability Information page", () => {
       cy.get('#submitReasonForLeave').click();
       cy.get('#fddFieldset').should('be.visible');
       cy.get('#ContentPlaceHolder1_ClaimantDisabilityTab_Dis1_rbConfFDDYes').click({ force: true });
+      cy.get('#reasonForLeavePage').should('not.exist')
       cy.get('#divLDW').should('be.visible');
     });
-    
+
     globalTestsNew(PAGE_ID, URL);
   });
 
