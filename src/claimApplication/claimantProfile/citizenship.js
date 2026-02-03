d861414fd3d9c5b014e5002093f9eb12790d3e23
Matt Luedke <matthew@innovation.nj.gov>
2026-02-03 11:52:57 -0800

    Personal analytics (#66)
    * track when phone number causes validation error
    
    * track every submit button click

--- src/claimApplication/claimantProfile/citizenship.js
+++ src/claimApplication/claimantProfile/citizenship.js
@@ -43,7 +43,7 @@ export function changes() {
   addRepresentativeTitle();
   removeWhitespaceNodes();
   removeLegend();
-  trackPhoneFirstThreeDigits();
+  trackSubmitBtnAndPhone();
   setNewTitle(i18next.t('citizenship.title'));
 }
 
@@ -136,6 +136,13 @@ function requirePhone() {
   asterisk.textContent = '* ';
   label.parentNode.insertBefore(asterisk, label);
 
+  const submitBtn = document.getElementById('ContentPlaceHolder1_ClaimantProfileTab_tpnlCitizen_btnSave');
+  submitBtn.addEventListener('click', () => {
+    if (phone1.validationMessage || phone2.validationMessage || phone3.validationMessage) {
+      logEvent("Personal Phone Omitted", {});
+    }
+  });
+
   const container = document.getElementById('ContentPlaceHolder1_ClaimantProfileTab_tpnlCitizen');
   const observer = new MutationObserver(updatePhoneFieldRequirements);
   observer.observe(container, { attributes: true });
@@ -214,11 +221,12 @@ function removeWhitespaceNodes() {
   });
 }
 
-function trackPhoneFirstThreeDigits() {
+function trackSubmitBtnAndPhone() {
   const submitBtn = document.getElementById('ContentPlaceHolder1_ClaimantProfileTab_tpnlCitizen_btnSave');
   const phoneFirstThreeDigits = document.getElementById('ContentPlaceHolder1_ClaimantProfileTab_tpnlCitizen_txtContactNum');
   if (submitBtn && phoneFirstThreeDigits) {
     submitBtn.addEventListener('click', function () {
+      logEvent("Contact information submit clicked", {});
       logEvent("Personal Phone Submitted", { first3: phoneFirstThreeDigits.value });
     });
   }
