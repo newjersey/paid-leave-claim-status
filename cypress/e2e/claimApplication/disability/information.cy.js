df97e32234ed7de5fdc2d0ae99786c7937b7dc7b
Matt Luedke <matthew@innovation.nj.gov>
2026-02-19 11:45:53 -0800

    Load reason for cpc from earlier field (#73)
    * update fixture and test to expect prior input to be loaded
    
    * reuse existing code but just change ID of element used
    
    * rename vendor to original

--- cypress/e2e/claimApplication/disability/information.cy.js
+++ cypress/e2e/claimApplication/disability/information.cy.js
@@ -11,7 +11,7 @@ import { encodeDecode } from '../../../../src/claimApplication/utils';
 const PAGE_ID = 'disabilityInformation';
 const URL = 'ClaimantDisabililty';
 const FIXTURE = "./cypress/fixtures/claimApplication/disability/information.html";
-const FIXTURE_WITH_ILLNESS = "./cypress/fixtures/claimApplication/disability/information_with_vendor_illness.html";
+const FIXTURE_WITH_ILLNESS = "./cypress/fixtures/claimApplication/disability/information_with_illness.html";
 
 describe("Disability Information page", () => {
   function checkPostData(interception) {
@@ -361,7 +361,7 @@ describe("Disability Information page", () => {
         .and('have.value', '');  
     });
 
-    it("parses illness with details from vendor field when no session data exists", () => {
+    it("parses illness with details from original field when no session data exists", () => {
       cy.visit(FIXTURE_WITH_ILLNESS);
       cy.wait('@script');
   
@@ -371,7 +371,7 @@ describe("Disability Information page", () => {
         .and('have.value', 'im hurt');
     });
   
-    it("saves parsed vendor data to session storage", () => {
+    it("saves parsed original data to session storage", () => {
       cy.visit(FIXTURE_WITH_ILLNESS);
       cy.wait('@script');
   
--- src/claimApplication/disability/reasonForLeave.js
+++ src/claimApplication/disability/reasonForLeave.js
@@ -283,7 +283,7 @@ export function restoreReasonForLeaveData(setDisabilityType) {
   let savedReason = savedData[STORAGE_KEY_REASON_FOR_LEAVE]
 
   if (!savedReason || !savedReason.reasons) {
-    savedReason = parseReasonFromVendorField();
+    savedReason = parseReasonFromOriginalField();
     if (!savedReason) {
       return false;
     }
@@ -333,9 +333,9 @@ export function restoreReasonForLeaveData(setDisabilityType) {
   }
 }
 
-function parseReasonFromVendorField() {
+function parseReasonFromOriginalField() {
   const originalTextarea = document.getElementById(
-    'ContentPlaceHolder1_ClaimantDisabilityTab_tabpnlDisabilityVerification_txtVerDisabDesc'
+    'ContentPlaceHolder1_ClaimantDisabilityTab_TabDoctor_txtInjury'
   );
 
   if (!originalTextarea || !originalTextarea.value) {
