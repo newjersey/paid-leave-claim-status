0a2985a42c27aea62724e197baba3ae3a2fa34d6
Abby Raskin <abbyraskin@gmail.com>
2026-02-24 18:39:45 -0600

    moved apostrophe

--- src/claimApplication/translations.js
+++ src/claimApplication/translations.js
@@ -235,7 +235,7 @@ Please submit the form by {{est_deadline_date}} to avoid delays on my claim. Ple
           inUSA: "Is your healthcare provider located in the United States?",
         },
         work: {
-          title: "Worker's Compensation",
+          title: "Workers' Compensation",
           causedByJob: "Was your disability caused by your job? (Could be a specific incident or happened over time)",
           workersCompClaim: `Have you or your employer filed a <a href="https://www.nj.gov/labor/workerscompensation/injured-worker-protections/index.shtml" target="_blank">Workers' Compensation Claim</a>, or do you plan to file one?`,
           workersCompClaimApproved: "Have you been approved for (or awarded, or received) Workers' Compensation benefits?",
