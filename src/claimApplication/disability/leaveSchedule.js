62c85dcace1a47952b254702eb228ebf2d197535
Matt Luedke <matthew@innovation.nj.gov>
2026-03-04 14:06:22 -0800

    replace icon

--- src/claimApplication/disability/leaveSchedule.js
+++ src/claimApplication/disability/leaveSchedule.js
@@ -11,6 +11,7 @@ export function showLeaveScheduleForDisabilityType(disabilityType) {
   styleFDD(disabilityType);
   styleLDW(disabilityType);
   addStyles();
+  replaceCalendarIcons();
 }
 
 function addStyles() {
@@ -290,4 +291,23 @@ function setupFutureDateAlert() {
     attributes: true,
     attributeFilter: ['style']
   });
+}
+
+function replaceCalendarIcons() {
+  var inputElement = document.getElementById('ContentPlaceHolder1_ClaimantDisabilityTab_Dis1_Image11');
+
+  var svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  svgElement.setAttribute('height', '24');
+  svgElement.setAttribute('viewBox', '0 0 24 24');
+  svgElement.setAttribute('width', '24');
+  svgElement.innerHTML = `
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M17 12h-5v5h5v-5zM16 1v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-1V1h-2zm3 18H5V8h14v11z"/>
+  `;
+
+  svgElement.setAttribute('alt', inputElement.getAttribute('alt'));
+  svgElement.setAttribute('onclick', inputElement.getAttribute('onclick'));
+
+  inputElement.parentNode.replaceChild(svgElement, inputElement);
 }
