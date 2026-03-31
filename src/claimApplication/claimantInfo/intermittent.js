5a910ce9bb7ab4432ce37d2b60e9edbd37f56d0c
Matt Luedke <matthew@innovation.nj.gov>
2026-03-31 10:11:30 -0700

    intermittent page calendars

--- src/claimApplication/claimantInfo/intermittent.js
+++ src/claimApplication/claimantInfo/intermittent.js
@@ -1,3 +1,5 @@
+import { updateCalendarUI } from '../utils';
+
 export const id = "intermittent";
 
 export const identifyingContent = {
@@ -18,6 +20,7 @@ export function changes() {
     fieldset2,
     'Since your disability began, have you been involved in a labor dispute with this employer?'
   );
+  updateAllCalendars();
 }
 
 function transformFieldset(fieldsetElement, questionText) {
@@ -53,4 +56,27 @@ function transformFieldset(fieldsetElement, questionText) {
     oldDiv.innerHTML = '';
     oldDiv.appendChild(newFieldset);
   }
+}
+
+function updateAllCalendars() {
+  updateCalendarUI("Image7");
+  updateCalendarUI("Image8");
+  updateCalendarUI("Image6");
+  updateCalendarUI("Image9");
+  updateCalendarUI("Image10");
+  updateCalendarUI("Image11");
+  updateCalendarUI("Image12");
+  updateCalendarUI("Image13");
+  updateCalendarUI("Image14");
+  updateCalendarUI("Image15");
+  updateCalendarUI("Image16");
+  updateCalendarUI("Image17");
+  updateCalendarUI("Image18");
+  updateCalendarUI("Image19");
+  updateCalendarUI("Image20");
+  updateCalendarUI("Image21");
+  updateCalendarUI("Image22");
+  updateCalendarUI("Image23");
+  updateCalendarUI("Image24");
+  updateCalendarUI("Image25");
 }
