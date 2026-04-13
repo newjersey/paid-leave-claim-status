7ca5437a3e8a97365ba92cd51066b67b86cc555c
Matt Luedke <matthew@innovation.nj.gov>
2026-04-13 13:56:16 -0700

    state or territory

--- src/claimApplication/translations.js
+++ src/claimApplication/translations.js
@@ -257,7 +257,7 @@ Please submit the form by {{est_deadline_date}} to avoid delays on my claim. Ple
         areYouReceivingOrApplied: "Since {{firstDayOfDisability}}, have you applied for or received any of the following?",
         areYouReceivingOrApplied_returned: "For the period from {{firstDayOfDisability}} to {{returnedToWorkDay}}, have you applied for or received any of the following?",
         pendingLabel: "My application is still pending",
-        stateLabel: "State where you filed:",
+        stateLabel: "State or territory where you filed:",
         dateLabel: "What time period do your benefits cover?",
         startLabel: "Start",
         endLabel: "End",
